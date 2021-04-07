b6611f73006fb9e556e53bb03cb74dd9a975eb3a
Bea <bebadmyweb@gmail.com>
2021-04-07 22:46:35 +0200

    utilitzacio props

--- src/book.js
+++ src/book.js
@@ -1,9 +1,20 @@
 import React from "react"
 
-function Book() {
+/*function Book() {
     return (
         <div>Viatge a la Lluna</div>
     )
-}
+}*/
+function Book(props) {
+    return <div>Viatge a la {props.name}</div>;
+  }
+  
+  function Book() {
+    return (
+      <div>
+        <Book name="Lluna" />
+      </div>
+    );
+  }
 export default Book
 
