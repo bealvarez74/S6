5de225c283abcba99557f267c4ad47e28a6ff86b
Usuari <usuari@MacBook-Pro-de-Usuari.local>
2021-04-21 16:45:59 +0200

    error Book.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Book from './Book'
 import booksData from './listBook'
 
 function App(){
-    const bookComponents = booksData.map (item => <Book key ={item.id} book={item}/>)
+    const bookComponents = booksData.map (item => <Book key={item.id} book={item}/>)
     
  return (
     <div>
